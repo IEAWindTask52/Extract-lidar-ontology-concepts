47540034bbac53382eab108ca7f4df3afc313b36
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-04-05 16:29:55 +0200

    including fields to update in function  "edit_yaml_from_ontology2"

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,22 +1,26 @@
-def edit_yaml_from_ontology(file_name,Lidar_Dictionary,nestedfields):
+def edit_yaml_from_ontology2(file_name,Lidar_Dictionary,nestedfields,fields2change):
     import ruamel.yaml
     str_nestedfields = 'config'
-    for inf in range (len(nestedfields)):   
-        str_nestedfields+='{}'.format(nestedfields[inf])
+    for inf0 in range (len(nestedfields)):   
+        str_nestedfields+='{}'.format(nestedfields[inf0])
 
 
     # Use the package ruamel to edit the yaml file    
     config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
     # Dictionary level where fields we want to change are located
     instances        = eval(str_nestedfields)    
+    for inf1 in range (len(fields2change)):   
+        str_fields2change='{}'.format(fields2change[inf1])    
+        
     
-    #Fields we want to change to the new values extracted from the ontology  
-    instances['Definition']        = Lidar_Dictionary['Definition']   
-    instances['Preferred Label']   = Lidar_Dictionary['Preferred Label']
-    instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
-    
-    yaml = ruamel.yaml.YAML()
-    yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
-    
-    with open(file_name, 'w') as fp:
-        yaml.dump(config, fp)
+        #Fields we want to change to the new values extracted from the ontology  
+        instances[str_fields2change]      = Lidar_Dictionary[str_fields2change]
+        #pdb.set_trace()
+        #instances['Preferred Label']   = Lidar_Dictionary['Preferred Label']
+        #instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
+
+        yaml = ruamel.yaml.YAML()
+        yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
+
+        with open(file_name, 'w') as fp:
+            yaml.dump(config, fp)
