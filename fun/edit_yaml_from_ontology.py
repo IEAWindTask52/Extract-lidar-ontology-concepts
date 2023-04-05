56b0c1a7f4d73d6fe03ac8864dad40b2165a1e49
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-04-05 13:21:39 +0200

    editing fields of the users yaml file

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,20 +1,20 @@
 def edit_yaml_from_ontology(file_name,Lidar_Dictionary):
     import ruamel.yaml
-    import pdb
-    # Use the package ruamel to edit the yaml file
-    pdb.set_trace()
+    
+    # Use the package ruamel to edit the yaml file    
     config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
-    pdb.set_trace()
+    
+    # Dictionary level where fields we want to change are located
     instances        = config['Components']['Scanner module']['Azimuth angle']    
-    pdb.set_trace()
-    instances['Definition']        = Lidar_Dictionary['Definition']
-    pdb.set_trace()
+    
+    #Fields we want to change to the new values extracted from the ontology  
+    instances['Definition']        = Lidar_Dictionary['Definition']   
     instances['Preferred Label']   = Lidar_Dictionary['Preferred Label']
     instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
-    pdb.set_trace()
+    
     yaml = ruamel.yaml.YAML()
     yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
-    pdb.set_trace()
+    
     with open(file_name, 'w') as fp:
         yaml.dump(config, fp)
     
