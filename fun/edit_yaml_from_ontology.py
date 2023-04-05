0f8500550f6eaa84cab5f95d367fb63d0dafa5cc
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-04-05 11:31:36 +0200

    Create edit_yaml_from_ontology.py

---
+++ fun/edit_yaml_from_ontology.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 10:18:16 2023
+
+@author: fcosta
+"""
+
+def edit_yaml_from_ontology(file_name):
+    # Use the package ruamel to edit the yaml file
+    config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
+        
+    instances = config['Components']['Scanner module']['Azimuth angle']    
+    instances['Definition'] = Lidar_Dictionary['Definition']
+    instances['Preferred Label'] = Lidar_Dictionary['Preferred Label']
+    instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
+    
+    yaml = ruamel.yaml.YAML()
+    yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
+    with open(file_name, 'w') as fp:
+        yaml.dump(config, fp)
+    
