9c6d3637ae12edb5d11d0b7a3dbacaa52c8ab2a2
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-04-05 17:41:52 +0200

    yaml editing

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -7,20 +7,23 @@ def edit_yaml_from_ontology(file_name,Lidar_Dictionary,nestedfields,fields2chang
 
     # Use the package ruamel to edit the yaml file    
     config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
+    
+    
+    
     # Dictionary level where fields we want to change are located
     instances        = eval(str_nestedfields)    
-    for inf1 in range (len(fields2change)):   
-        str_fields2change='{}'.format(fields2change[inf1])    
-        
-    
-        #Fields we want to change to the new values extracted from the ontology  
-        instances[str_fields2change]      = Lidar_Dictionary[str_fields2change]
-        #pdb.set_trace()
-        #instances['Preferred Label']   = Lidar_Dictionary['Preferred Label']
-        #instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
+    # for inf1 in range (len(fields2change)):   
+    str_fields2change1='{}'.format(fields2change[0])  # str_fields2change is the field   
+    str_fields2change2='{}'.format(fields2change[1])
+    str_fields2change3='{}'.format(fields2change[2])
+    #Fields we want to change to the new values extracted from the ontology  
+    instances[str_fields2change1]      = Lidar_Dictionary[str_fields2change1]
+    instances[str_fields2change2]      = Lidar_Dictionary[str_fields2change2]
+    instances[str_fields2change3]      = Lidar_Dictionary[str_fields2change3]
 
-        yaml = ruamel.yaml.YAML()
-        yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
 
-        with open(file_name, 'w') as fp:
-            yaml.dump(config, fp)
+    yaml = ruamel.yaml.YAML()
+    yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
+
+    with open(file_name, 'w') as fp:
+        yaml.dump(config, fp)
