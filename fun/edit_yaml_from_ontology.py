64458928dfc688ccf200b82a9f9dce776f27d48b
Francisco Costa <51125855+PacoCosta@users.noreply.github.com>
2023-04-05 13:07:12 +0200

    Update edit_yaml_from_ontology.py

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,15 +1,19 @@
 def edit_yaml_from_ontology(file_name):
-    
+    import pdb
     # Use the package ruamel to edit the yaml file
+    pdb.set_trace()
     config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
+    pdb.set_trace()
     instances        = config['Components']['Scanner module']['Azimuth angle']    
-    
+    pdb.set_trace()
     instances['Definition']        = Lidar_Dictionary['Definition']
+    pdb.set_trace()
     instances['Preferred Label']   = Lidar_Dictionary['Preferred Label']
     instances['Alternative Label'] = Lidar_Dictionary['Alternative Label']
-    
+    pdb.set_trace()
     yaml = ruamel.yaml.YAML()
     yaml.indent(mapping=ind, sequence=ind, offset=bsi) 
+    pdb.set_trace()
     with open(file_name, 'w') as fp:
         yaml.dump(config, fp)
     
