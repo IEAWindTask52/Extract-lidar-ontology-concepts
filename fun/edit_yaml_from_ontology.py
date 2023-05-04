98bb5e3fdc93fe9ba7d58e5287d79468341f2ed3
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-05-05 01:46:25 +0200

    .edit function

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,19 +1,25 @@
-def find(obj,tag,field):    
-    pdb.set_trace()
+
+
+def find(obj,tag):    
     if tag in obj:
         yield obj[tag]
     for k,v in obj.items():       
         if isinstance(v,dict):
-            for i in find(v,tag,field):
+            for i in find(v,tag):
                 yield i                
+
+def edit(local_yaml,tag,fields2change,Lidar_Dictionary):
+    import ruamel.yaml
+    yaml = ruamel.yaml.YAML()
+    with open (local_yaml,"r") as file:
+        data = yaml.load_all(file)
+        for obj in data:
+            for val in find(obj,tag):
+                for i in fields2change:
+                    val[i]=Lidar_Dictionary[i]
     
-    # with open (Lidar_inputs,"r") as file:
-    #     data = yaml.load_all(file)
-    #     for obj in data:
-    #         for val in find(obj,tag,field):
-    #             for i in field:
-    #                 val[i]=Lidar_Dictionary[i]
+    # Edit the desired fields of the local file
+    with open(local_yaml, "w") as file:
+        yaml.dump(obj, file)
     
-    # with open(Lidar_inputs, "w") as file:
-    #     #pdb.set_trace()
-    #     yaml.dump(obj, file)
+    
