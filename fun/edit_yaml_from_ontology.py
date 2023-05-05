a227b60306c6d1bda3cb9c8020630567b03a95d0
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-05-05 02:12:11 +0200

    .edit function
    edits the local yaml file with ontology info

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,5 +1,3 @@
-
-
 def find(obj,tag):    
     if tag in obj:
         yield obj[tag]
@@ -11,12 +9,15 @@ def find(obj,tag):
 def edit(local_yaml,tag,fields2change,Lidar_Dictionary):
     import ruamel.yaml
     yaml = ruamel.yaml.YAML()
+    
+    # Read and assign the desired fields of the downloaded concept     
     with open (local_yaml,"r") as file:
         data = yaml.load_all(file)
         for obj in data:
             for val in find(obj,tag):
                 for i in fields2change:
-                    val[i]=Lidar_Dictionary[i]
+                    if not val[i]:
+                        val[i]=Lidar_Dictionary[i]
     
     # Edit the desired fields of the local file
     with open(local_yaml, "w") as file:
