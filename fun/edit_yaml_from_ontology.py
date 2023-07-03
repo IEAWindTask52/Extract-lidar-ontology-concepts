a220885718ce700f97446eb08b9072f2af030979
Francisco Costa <51125855+PacoCosta@users.noreply.github.com>
2023-07-03 17:53:29 +0200

    Adding explanation to functions

--- fun/edit_yaml_from_ontology.py
+++ fun/edit_yaml_from_ontology.py
@@ -1,4 +1,18 @@
-def find(obj,tag):    
+def find(obj,tag):  
+""".   
+Function digging into the local YAML file to find 
+the fetched concept and the field(s) to be updated:      
+    
+Inputs
+------- 
+* obj:
+    Each element contained in the local YAML file
+* tag:
+    Fields of the ontology concept to be updated  
+Returns
+-------
+String    
+"""
     if tag in obj:
         yield obj[tag]
     for k,v in obj.items():       
@@ -7,6 +21,23 @@ def find(obj,tag):
                 yield i                
 
 def edit(local_yaml,tag,fields2change,Lidar_Dictionary):
+""".   
+Opens, updates, saves, and closes the local YAML file.    
+    
+Inputs
+-------    
+* local_yaml:
+    Local YAML file to be updated
+* tag:
+    Concept to be updated   
+* fields2change:
+    Fields to be updated 
+* Lidar_Dictionary:
+    dict-like output object from the Github tool
+Returns
+-------
+YAML file    
+"""
     import ruamel.yaml
     yaml = ruamel.yaml.YAML()
     
